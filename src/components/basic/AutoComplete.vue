df6fe68ee7509944fc60098e016d5e59bdb4cda5
SeanRivardMorton <sean.rivard.morton@gmail.com>
2018-06-10 11:23:19 -0400

    Add Autocomplete component

---
+++ src/components/basic/AutoComplete.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container fluid>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-select :items="items" v-model="autoComplete" label="Select" autocomplete></v-select>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class AutoComplete extends Vue {
+    @Prop() items!: string;
+    autoComplete = null;
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,44 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-  </div>
+  <v-layout justify-center>
+    <v-flex xs12 lg6>
+      <v-card>
+        <v-card-title class="title">
+          {{ title }}
+        </v-card-title>
+        <auto-complete :items="SongNames"></auto-complete>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Getter, Action } from 'vuex-class';
+import AutoComplete from '@/components/basic/AutoComplete.vue'
 
-
-@Component({})
+@Component({
+  components: {
+    AutoComplete
+  }
+})
 export default class Home extends Vue {
+  title: string = 'Search Songs';
   @Getter('topSongs') topSongs!: any;
   @Action('setTopSongs') setTopSongs!: null;
+  items = [1,2,3,4]
+
+  get SongNames() {
+    return this.topSongs ? this.topSongs.feed.entry.map( item => item.title.label ) : []
+  }
 
   created() {
-    // Load in the top songs
     this.setTopSongs();
   }
 }
 </script>
+
+<style lang="scss">
+// .card {
+//   top: 30vh;
+// }
+</style>
