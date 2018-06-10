8278e3fa0d3b1542ef09d994b13492552cb2560e
SeanRivardMorton <sean.rivard.morton@gmail.com>
2018-06-10 11:28:49 -0400

    Added navigation bar to layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <v-app dark>
+      <v-toolbar fixed app :clipped-left="clipped">
+        <v-toolbar-side-icon color="red" @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+      </v-toolbar>
       <v-content>
         <v-container>
           <router-view/>
@@ -21,7 +25,7 @@ export default class Home extends Vue {
   miniVariant: boolean = false;
   right: boolean = false;
   rightDrawer: boolean = false;
-  title: string = 'CopoFi Submission';
+  title: string = 'itunes Top 100';
 
   get drawerProperties() {
     return {
